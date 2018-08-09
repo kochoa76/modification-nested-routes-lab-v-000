0f8820d3bbc526689f044db82b29be3398d4b9cd
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-09 22:16:50 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
      else
-       artist.name
+       song.artist.name
     end
   end
 end
